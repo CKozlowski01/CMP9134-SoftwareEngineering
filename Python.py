a4bf494ad12b5528a170ba900374659baa709794
Cezary Kozlowski <cezarykozlowski13@gmail.com>
2023-03-21 18:29:08 +0000

    Transfer Page Coded

--- Python.py
+++ Python.py
@@ -20,7 +20,7 @@ class BankingController(tk.Tk):
         self.frames = {}
 
         #Iterate through all frames
-        for F in (LoginPage, CreateAnAccount, HomePage):
+        for F in (LoginPage, CreateAnAccount, HomePage, TransferPage):
             #Find frame name
             page_name = F.__name__
 
@@ -74,6 +74,45 @@ class HomePage(tk.Frame):
 
     def __init__(self, cont, controller):
         tk.Frame.__init__(self, cont)
+
+class TransferPage(tk.Frame):
+
+    def __init__(self, cont, controller):
+        tk.Frame.__init__(self, cont, bg="white")
+        titleLabel = tk.Label(self, text="Money Safe", height=2, font=("Times New Roman",64),borderwidth=3, relief="solid",bg="#016846", fg="white")
+        titleLabel.pack(side="top", fill="x")
+
+        accountList = ("option 1", "option 2", "option 3")
+        self.v = tk.StringVar()
+        self.v.set(accountList[0])
+        selectBox = tk.OptionMenu(self, self.v, *accountList)
+        selectBox.config(justify=CENTER, width=60, font=("Times New Roman",24),borderwidth=3, relief="solid",bg="#016846", fg="white")
+        selectBox["menu"].config(font=("Times New Roman",24),borderwidth=3, relief="solid",bg="#016846", fg="white")
+        selectBox.pack(side="top", pady=(100, 10))
+
+        backBtn = tk.Button(self, text="Back", width=10, font=("Times New Roman",24),borderwidth=3, relief="solid",bg="#016846", fg="white")
+        backBtn.pack(side="bottom", pady=(0,50), padx=(0, 1000))
+
+        transferBtn = tk.Button(self, text="Transfer", width=40, font=("Times New Roman",24),borderwidth=3, relief="solid",bg="#016846", fg="white")
+        transferBtn.pack(side="bottom", pady=(0, 100))
+
+        transferTextbox = tk.Entry(self, justify=CENTER, width=20, font=("Times New Roman",24),borderwidth=3, relief="solid",bg="#016846", fg="white")
+        transferTextbox.insert(INSERT, "Transfer Amount")
+        transferTextbox.pack(side="left", padx=(300, 0), pady=(0, 100))
+
+        transferAccTextbox = tk.Entry(self, justify=CENTER, width=20, font=("Times New Roman",24),borderwidth=3, relief="solid",bg="#016846", fg="white")
+        transferAccTextbox.insert(INSERT, "Recievers Username")
+        transferAccTextbox.pack(side="right", padx=(0, 300), pady=(0, 100))
+
+        
+        # usernameTextbox = tk.Entry(self, justify=CENTER, width=60, font=("Times New Roman",24),borderwidth=3, relief="solid",bg="#016846")
+        # usernameTextbox.insert(INSERT, "Username")
+        # usernameTextbox.pack(side="top", pady=(250,25))
+
+        # selection = tk.StringVar()
+        # selectBox = ttk.Combobox(self, text="Select Account", textvariable=selection, values = ["option 1", "option 2", "option 3"], justify=CENTER, width=60, font=("Times New Roman",24))
+        # selectBox.pack(side="top", pady="100")
+
 
 if __name__ == "__main__":
     app = BankingController()
